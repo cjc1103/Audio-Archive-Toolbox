fdb3c3c5d9a55dc1ef5ebd110fd6cf0e422a796a
Chris Cantwell <cjcantwell@outlook.com>
2022-12-11 18:40:29 -0500

    Corrected search for Album in cuesheet

--- GetDirMetadata.cs
+++ GetDirMetadata.cs
@@ -382,7 +382,7 @@ namespace AATB
 
                 // search for standard metadata labels
                 Dir.AlbumArtist = SearchList(DataList, "PERFORMER ");
-                Dir.Album = SearchList(DataList, "TITLE");
+                Dir.Album = SearchList(DataList, "AlBUM");
                 Dir.Event = SearchList(DataList, "EVENT ");
                 Dir.Venue = SearchList(DataList, "VENUE ");
                 Dir.Stage = SearchList(DataList, "STAGE ");
